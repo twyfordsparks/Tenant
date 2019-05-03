8aef4037ca0eacd02020d280d08c426a26867954
OkothPius <orukopius8@gmail.com>
2019-05-03 10:53:13 +0300

    create classes linkiking to db

---
+++ app/models.py
@@ -0,0 +1,129 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    tenants = db.relationship("Tenants", backref="user", lazy="dynamic")
+    comment = db.relationship("Comments", backref="user", lazy="dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+class TenantCategory(db.Model):
+    '''
+    Category class define category per tenant
+    '''
+    __tablename__ = 'tenant_categories'
+
+    # table columns
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+
+    # save tenants
+    def save_category(self):
+        '''
+        Function that saves a category
+        '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_categories(cls):
+        '''
+        Function that returns all the data from the categories after being queried
+        '''
+        categories = TenantCategory.query.all()
+        return categories
+
+
+class Tenants(db.Model):
+
+    """
+    Class that holds instances of all the tenants in the different categories
+    """
+    all_tenants = []
+
+    __tablename__ = 'tenants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    actual_tenant = db.Column(db.String)
+    date_posted = db.Column(db.DateTime, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    category_id = db.Column(db.Integer, db.ForeignKey("tenant_categories.id"))
+    comment = db.relationship("Comments", backref="tenants", lazy="dynamic")
+
+    def save_tenant(self):
+        '''
+        Function to save a tenant
+        '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def clear_tenants(cls):
+        """Function which clears all the tenants in a particular category"""
+        Tenants.all_tenants.clear()
+
+    # display tenants
+    @classmethod
+    def get_pittenantches(cls, id):
+        """Function which gets a particular tenant when requested by date posted"""
+        tenants = Tenants.query.order_by(Tenants.date_posted.desc()).filter_by(category_id=id).all()
+        return tenants
+
+
+class Comments(db.Model):
+    '''
+    Comment class that creates instances of Comments class that will be attached to a particular pi
+    '''
+    __tablename__ = 'comment'
+
+    # add columns
+    id = db.Column(db. Integer, primary_key=True)
+    comment_id = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    tenants_id = db.Column(db.Integer, db.ForeignKey("tenants.id"))
+
+    def save_comment(self):
+        '''
+        Save the comments per tenant
+        '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(self, id):
+        comment = Comments.query.order_by(
+            Comments.date_posted.desc()).filter_by(tenants_id=id).all()
+        return comment
